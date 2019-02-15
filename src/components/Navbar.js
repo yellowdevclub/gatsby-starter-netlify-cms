d3739aa51aba1fd52fa5f0d44a6d40dca278bc70
yellowdevclub <47618937+yellowdevclub@users.noreply.github.com>
2019-02-15 20:23:53 +0900

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
+import logoyellowdev from '../img/logo_yellowDEV.svg'
 
 const Navbar = class extends React.Component {
 
@@ -35,7 +36,7 @@ const Navbar = class extends React.Component {
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item" title="Logo">
-          <img src={logo} alt="yellowDEV" style={{ max-width: '240px' }} />
+          <img src={logoyellowdev} alt="yellowDEV" style={{ max-width: '240px' }} />
         </Link>
         {/* Hamburger menu */}
         <div className="navbar-burger burger" data-target="navMenu">
